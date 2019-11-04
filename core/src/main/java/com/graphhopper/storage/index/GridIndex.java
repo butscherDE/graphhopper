40b8cbdde72fdd1cb117d6d9836320e9da8e18ff
Daniel Metzger <daniel.metzger@uni-konstanz.de>
2019-11-04 21:30:44 +0100

    wip: Implemented findClosest

--- core/src/main/java/com/graphhopper/storage/index/GridIndex.java
+++ core/src/main/java/com/graphhopper/storage/index/GridIndex.java
@@ -3,6 +3,10 @@ package com.graphhopper.storage.index;
 import com.graphhopper.routing.util.AllEdgesIterator;
 import com.graphhopper.routing.util.EdgeFilter;
 import com.graphhopper.storage.*;
+import com.graphhopper.util.DistanceCalc;
+import com.graphhopper.util.EdgeIterator;
+import com.graphhopper.util.EdgeIteratorState;
+import com.graphhopper.util.Helper;
 import com.graphhopper.util.shapes.BBox;
 
 import java.util.ArrayList;
@@ -12,6 +16,7 @@ import java.util.List;
 public class GridIndex implements LocationIndex {
     private final static double MAX_LATITUDE = 90;
     private final static double MAX_LONGITUDE = 180;
+    private final static DistanceCalc DISTANCE_CALCULATOR = Helper.DIST_PLANE;
 
     private final Graph graph;
     private final NodeAccess nodeAccess;
@@ -132,7 +137,7 @@ public class GridIndex implements LocationIndex {
 
     @Override
     public QueryResult findClosest(double lat, double lon, EdgeFilter edgeFilter) {
-        return null;
+        return new QueryResultCreator(lat, lon).createQueryResult();
     }
 
     @Override
@@ -219,6 +224,191 @@ public class GridIndex implements LocationIndex {
         public GridCell(final BBox boundingBox) {
             this.nodes = new ArrayList<>();
             this.boundingBox = boundingBox;
+        }
+    }
+
+    private class QueryResultCreator {
+        private final double queryLatitude;
+        private final double queryLongitude;
+        private final QueryResult queryResult;
+
+        private int closestNode = -1;
+        private double closestNodeDistance = Double.MAX_VALUE;
+        private EdgeIteratorState closestEdge = null;
+        private double closestEdgeDistance = Double.MAX_VALUE;
+        private GridCell[] currentSearchSpace;
+        private int latitudeIndex;
+        private int longitudeIndex;
+        private int[] latitudeIndices;
+        private int[] longitudeIndices;
+
+        public QueryResultCreator(final double queryLatitude, final double queryLongitude) {
+            this.queryLatitude = queryLatitude;
+            this.queryLongitude = queryLongitude;
+            this.queryResult = new QueryResult(queryLatitude, queryLongitude);
+        }
+
+        public QueryResult createQueryResult() {
+            findClosestNode();
+            findClosestEdge();
+            addValuesToQueryResult();
+
+            return queryResult;
+        }
+
+        private void findClosestNode() {
+            createIntialSearchSpace();
+
+            while (closestNodeNotFound() && searchSpaceNotExhausted()) {
+                findClosestNodeInSourrondingGridCells();
+
+                setEnlargedIndices();
+                setEnlargedSearchSpace();
+            }
+        }
+
+        private boolean closestNodeNotFound() {
+            return closestNode == -1;
+        }
+
+        private boolean searchSpaceNotExhausted() {
+            return this.longitudeIndices.length < 2 * resolution;
+        }
+
+        private void createIntialSearchSpace() {
+            latitudeIndex = getIndexByCoordinate(queryLatitude, MAX_LATITUDE);
+            longitudeIndex = getIndexByCoordinate(queryLongitude, MAX_LONGITUDE);
+
+            currentSearchSpace = getGridCellQueriedAndItsNeighbors(latitudeIndex, longitudeIndex);
+        }
+
+        private void findClosestNodeInSourrondingGridCells() {
+            for (final GridCell cell : currentSearchSpace) {
+                if (cell == null) {
+                    System.out.println();
+                }
+
+                for (final int otherNode : cell.nodes) {
+                    updateClosestNodeIfCloser(otherNode);
+                }
+            }
+        }
+
+        private void updateClosestNodeIfCloser(int otherNode) {
+            final double otherNodeLatitude = nodeAccess.getLatitude(otherNode);
+            final double otherNodeLongitude = nodeAccess.getLongitude(otherNode);
+
+            final double distance = DISTANCE_CALCULATOR.calcNormalizedDist(queryLatitude, queryLongitude, otherNodeLatitude, otherNodeLongitude);
+            if (distance < closestNodeDistance) {
+                closestNode = otherNode;
+                closestNodeDistance = distance;
+            }
+        }
+
+        private void setEnlargedIndices() {
+            this.latitudeIndices = getEnlargedIndices(this.latitudeIndices);
+            this.longitudeIndices = getEnlargedIndices(this.longitudeIndices);
+        }
+
+        private int[] getEnlargedIndices(final int[] indices) {
+            final int[] newIndices = new int[indices.length + 2];
+
+            newIndices[0] = checkAndCorrectIndexForOutOfBounds(indices[0] - 1);
+            System.arraycopy(indices, 0, newIndices, 1, indices.length);
+            newIndices[newIndices.length - 1] = checkAndCorrectIndexForOutOfBounds(indices[indices.length - 1] + 1);
+
+            return newIndices;
+        }
+
+        private GridCell[] getGridCellQueriedAndItsNeighbors(int latitudeIndex, int longitudeIndex) {
+            getInitialLatIndices(latitudeIndex);
+            getInitialLonIndices(latitudeIndex);
+
+            return new GridCell[]{index[latitudeIndices[0]][longitudeIndices[0]],
+                                  index[latitudeIndices[0]][longitudeIndices[1]],
+                                  index[latitudeIndices[0]][longitudeIndices[2]],
+                                  index[latitudeIndices[1]][longitudeIndices[0]],
+                                  index[latitudeIndices[1]][longitudeIndices[1]],
+                                  index[latitudeIndices[1]][longitudeIndices[2]],
+                                  index[latitudeIndices[2]][longitudeIndices[0]],
+                                  index[latitudeIndices[2]][longitudeIndices[1]],
+                                  index[latitudeIndices[2]][longitudeIndices[2]]
+            };
+        }
+
+        private void getInitialLatIndices(int latitudeIndex) {
+            latitudeIndices = new int[] {checkAndCorrectIndexForOutOfBounds(latitudeIndex - 1),
+                                                             latitudeIndex,
+                                                             checkAndCorrectIndexForOutOfBounds(latitudeIndex + 1)};
+
+        }
+
+        private void getInitialLonIndices(int longitudeIndex) {
+            longitudeIndices = new int [] {checkAndCorrectIndexForOutOfBounds(longitudeIndex - 1),
+                                                              longitudeIndex,
+                                                              checkAndCorrectIndexForOutOfBounds(longitudeIndex + 1)};
+        }
+
+        private void setEnlargedSearchSpace() {
+            final GridCell[] newSearchSpace = new GridCell[numberOfCurrentSearchSpaceSurroundingCells()];
+
+            int i = 0;
+            for (int j = 0; j < latitudeIndices.length; j++) {
+                newSearchSpace[i++] = index[latitudeIndices[j]][longitudeIndices[0]];
+                newSearchSpace[i++] = index[latitudeIndices[j]][longitudeIndices[longitudeIndices.length - 1]];
+            }
+            for (int j = 1; j < longitudeIndices.length - 1; j++) {
+                newSearchSpace[i++] = index[latitudeIndices[0]][longitudeIndices[j]];
+                newSearchSpace[i++] = index[latitudeIndices[latitudeIndices.length - 1]][longitudeIndices[j]];
+            }
+
+            this.currentSearchSpace = newSearchSpace;
+        }
+
+        private int numberOfCurrentSearchSpaceSurroundingCells() {
+            return latitudeIndices.length * 4  - 4;
+        }
+
+        private int checkAndCorrectIndexForOutOfBounds(final int index) {
+            final int minIndex = 0;
+            final int maxIndex = resolution - 1;
+
+            int correctIndex = index;
+            if (correctIndex > maxIndex) {
+                correctIndex = minIndex;
+            } else if (correctIndex < minIndex) {
+                correctIndex = maxIndex;
+            }
+
+            return correctIndex;
+        }
+
+        private void findClosestEdge() {
+            checkAllNeighborsOfClosestNodeForClosestEdge();
+        }
+
+        private void checkAllNeighborsOfClosestNodeForClosestEdge() {
+            final EdgeIterator neighbourFinder = graph.createEdgeExplorer().setBaseNode(this.closestNode);
+            while (neighbourFinder.next()) {
+                updateClosestEdgeIfCloser(neighbourFinder);
+            }
+        }
+
+        private void updateClosestEdgeIfCloser(EdgeIterator neighbourFinder) {
+            final double neighbourLatitude = nodeAccess.getLatitude(neighbourFinder.getAdjNode());
+            final double neighbourLongitude = nodeAccess.getLongitude(neighbourFinder.getAdjNode());
+
+            final double distance = DISTANCE_CALCULATOR.calcNormalizedDist(queryLatitude, queryLongitude, neighbourLatitude, neighbourLongitude);
+            if (distance < closestEdgeDistance) {
+                closestEdge = neighbourFinder.detach(false);
+                closestEdgeDistance = distance;
+            }
+        }
+
+        private void addValuesToQueryResult() {
+            queryResult.setClosestNode(this.closestNode);
+            queryResult.setQueryDistance(this.closestNodeDistance);
+            queryResult.setClosestEdge(this.closestEdge);
         }
     }
 }
