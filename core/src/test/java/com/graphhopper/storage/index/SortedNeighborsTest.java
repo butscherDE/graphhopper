76222106604ba85d0c823e2292ee5dc94162d27c
Daniel Metzger <daniel.metzger@uni-konstanz.de>
2020-01-14 10:57:47 +0100

    added SortedNeighbors test with multi edges in collinear in-edges

--- core/src/test/java/com/graphhopper/storage/index/SortedNeighborsTest.java
+++ core/src/test/java/com/graphhopper/storage/index/SortedNeighborsTest.java
@@ -1,10 +1,16 @@
 package com.graphhopper.storage.index;
 
+import com.graphhopper.routing.template.util.Edge;
+import com.graphhopper.routing.template.util.Node;
 import com.graphhopper.routing.template.util.PolygonRoutingTestGraph;
 import com.graphhopper.util.EdgeIteratorState;
+import com.graphhopper.util.shapes.Polygon;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class SortedNeighborsTest {
     private final static PolygonRoutingTestGraph GRAPH_MOCKER = new PolygonRoutingTestGraph();
@@ -18,16 +24,65 @@ public class SortedNeighborsTest {
         assertOrdering(expectedOrder, sortedNeighbors);
     }
 
+    @Test
+    public void orderingWithMultiEdges() {
+        final PolygonRoutingTestGraph graphMocker = getMultiEdgeTestGraph();
+
+        final SortedNeighbors sortedNeighbors = getSortedNeighbors(graphMocker, 1, 0);
+
+        assertEquals(0, sortedNeighbors.get(0).getEdge());
+        assertEquals(1, sortedNeighbors.get(1).getEdge());
+    }
+
+    private PolygonRoutingTestGraph getMultiEdgeTestGraph() {
+        final Node[] nodes = new Node[] {
+                new Node(0, 0, 0),
+                new Node(1, 0, 1)
+        };
+        final Edge[] edges = new Edge[] {
+                new Edge(0, 1, 1, true),
+                new Edge(0, 1, 1, true)
+        };
+        return new PolygonRoutingTestGraph(nodes, edges);
+    }
+
+    @Test
+    public void collinearInEdges() {
+        final PolygonRoutingTestGraph graphMocker = getCollinearInEdgesTestGraph();
+
+        final SortedNeighbors sortedNeighbors = getSortedNeighbors(graphMocker, 2, 0);
+
+        final int[] expectedOrder = new int[] {0, 1, 3};
+
+        assertOrdering(expectedOrder, sortedNeighbors);
+    }
+
+    private PolygonRoutingTestGraph getCollinearInEdgesTestGraph() {
+        final Node[] nodes = new Node[] {
+                new Node(0, 0, 0),
+                new Node(1, 0, 1),
+                new Node(2, 0, 2),
+                new Node(3, 0, 3)
+        };
+        final Edge[] edges = new Edge[] {
+                new Edge(0, 2, 1, true),
+                new Edge(1, 2, 1, true),
+                new Edge(2, 3, 1, true)
+        };
+        return new PolygonRoutingTestGraph(nodes, edges);
+    }
+
     private SortedNeighbors getSortedNeighbors(final PolygonRoutingTestGraph graphMocker, final int baseNode, final int adjNode) {
         final VectorAngleCalculator vac = new VectorAngleCalculatorLeft(graphMocker.nodeAccess);
 
-        final EdgeIteratorState lastEdgeReversed = graphMocker.getEdge(57, 52);
-        return new SortedNeighbors(vac, GRAPH_MOCKER.graph, lastEdgeReversed.getBaseNode(), lastEdgeReversed);
+        final EdgeIteratorState lastEdgeReversed = graphMocker.getEdge(baseNode, adjNode);
+        return new SortedNeighbors(vac, graphMocker.graph, lastEdgeReversed.getBaseNode(), lastEdgeReversed);
     }
 
     private void assertOrdering(int[] expectedOrder, SortedNeighbors sortedNeighbors) {
         final int[] actualOrder = extractAdjNodes(sortedNeighbors);
-
+        System.out.println("Expected: " + Arrays.toString(expectedOrder));
+        System.out.println("Actual: " + Arrays.toString(actualOrder));
         assertArrayEquals(expectedOrder, actualOrder);
     }
 
