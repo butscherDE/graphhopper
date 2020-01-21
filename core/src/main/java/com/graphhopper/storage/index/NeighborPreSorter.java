69d2128b4d06aee39586583b6726c5c1fd9fd29a
Daniel Metzger <daniel.metzger@uni-konstanz.de>
2020-01-21 10:45:34 +0100

    added mem kill test

--- core/src/main/java/com/graphhopper/storage/index/NeighborPreSorter.java
+++ core/src/main/java/com/graphhopper/storage/index/NeighborPreSorter.java
@@ -2,11 +2,9 @@ package com.graphhopper.storage.index;
 
 import com.graphhopper.storage.Graph;
 import com.graphhopper.util.EdgeIterator;
+import com.graphhopper.util.EdgeIteratorState;
 
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class NeighborPreSorter {
     private final Graph graph;
@@ -34,8 +32,28 @@ public class NeighborPreSorter {
     private void createSortedNeighbors() {
         final Set<Integer> allNodes = getAllNodes();
 
+//        testMemKill(allNodes);
+//        System.out.println("#!#!#!");
+//        try {
+//            Thread.sleep(10_000);
+//        } catch (Exception e) {
+//
+//        }
+
         addSortedNeighbors(allNodes);
     }
+
+//    private List<EdgeIteratorState> allEdges = new LinkedList<>();
+//    private void testMemKill(final Set<Integer> allNodes) {
+//        for (Integer allNode : allNodes) {
+//            final EdgeIterator neighbors = graph.createEdgeExplorer().setBaseNode(allNode);
+//            while(neighbors.next()) {
+//                allEdges.add(neighbors.detach(false));
+//                final double lat = graph.getNodeAccess().getLat(neighbors.getAdjNode());
+//                final double lon = graph.getNodeAccess().getLon(neighbors.getAdjNode());
+//            }
+//        }
+//    }
 
     private Set<Integer> getAllNodes() {
         final Set<Integer> allNodes = new LinkedHashSet<>();
@@ -55,7 +73,24 @@ public class NeighborPreSorter {
             allSortedNeighborsLeft.put(node, sortedNeighborsLeft);
             final SortedNeighbors sortedNeighborsRight = new SortedNeighbors(graph, node, SortedNeighbors.DO_NOT_IGNORE_NODE, new VectorAngleCalculatorRight(graph.getNodeAccess()));
             allSortedNeighborsRight.put(node, sortedNeighborsRight);
-            System.out.println(i++ / (double) allNodes.size() + " % processed");
+
+            if (i % 1_000_000 == 0) {
+                System.out.println(i + ": " + i / (double) allNodes.size() + " % processed, global size: " + estimateSize());
+            }
+            i++;
         }
+    }
+
+    private int estimateSize() {
+        int globalSize = 0;
+
+        for (Map.Entry<Integer, SortedNeighbors> entry : allSortedNeighborsLeft.entrySet()) {
+            globalSize += entry.getValue().size();
+        }
+        for (Map.Entry<Integer, SortedNeighbors> entry : allSortedNeighborsRight.entrySet()) {
+            globalSize += entry.getValue().size();
+        }
+
+        return globalSize;
     }
 }
